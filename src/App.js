d3b30f073cf5093c1e78c59c743f7a5791d61e0b
bobrowiczkrzysztof <69210846+bobrowiczkrzysztof@users.noreply.github.com>
2021-12-03 19:02:42 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+let i = 0;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: '',
+      author: '',
+      previousQuotes: [],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch(
+      'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
+    )
+      .then(response => response.json())
+      .then(data => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        return this.setState({
+          quote: data[randomNum]['quote'],
+          author: data[randomNum]['author'],
+        });
+      })
+      .catch(err =>
+        this.setState({
+          quote: 'Error: something is wrong. Please try again',
+          author: 'unknown',
+        })
+      );
+  }
+  getNextQuote() {
+    this.componentDidMount();
+    if (
+      !this.state.previousQuotes.some(obj => obj.quote === this.state.quote)
+    ) {
+      this.setState(state => ({
+        previousQuotes: state.previousQuotes.concat({
+          quote: state.quote,
+          author: state.author,
+        }),
+      }));
+    }
+    i = this.state.previousQuotes.length + 1;
+  }
+  getPreviousQuote() {
+    if (i !== 0 && this.state.previousQuotes.length !== 0) {
+      --i;
+      if (this.state.previousQuotes[i] !== undefined) {
+        this.setState(state => ({
+          quote: state.previousQuotes[i].quote,
+          author: state.previousQuotes[i].author,
+        }));
+      }
+    }
+  }
+  render() {
+    return (
+      <div id="container">
+        <div id="quotes-container">
+          <q id="text">{this.state.quote}</q>
+          <p id="author">-{this.state.author}</p>
+          <div>
+            <button id="next-quote" onClick={this.getNextQuote.bind(this)}>
+              Next quote
+            </button>
+            <button
+              id="previous-quote"
+              onClick={this.getPreviousQuote.bind(this)}
+            >
+              Previous quotes
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
